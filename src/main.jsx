143a4917b87cedadde94e4627020fd0232afa360
David Kwon <david.kwon@clover.com>
2024-10-15 12:40:09 -0600

    Add background gradient

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import Bio from "./routes/Bio.jsx";
 import Experience from "./routes/Experience.jsx";
 import Portfolio from "./routes/Portfolio.jsx";
-import Nav from "./Nav.jsx";
+import Nav from "./components/Nav.jsx";
 import "./index.css";
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 
