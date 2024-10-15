861d9b3a948250cbb87fc2ce25a5588e6aece41c
David Kwon <david.kwon@clover.com>
2024-10-15 15:28:36 -0600

    Add portfolio link

--- src/routes/Portfolio.jsx
+++ src/routes/Portfolio.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
+    <section className='h-[85%] w-full flex justify-center items-center'>
+      <p className='text-xl mb-4 text-white'>Check out more of my work in my <a href='https://dkwon.vercel.app/' className='text-blue-600 dark:text-blue-500 hover:underline'>portfolio</a></p>
+    </section>
   )
 }
 
