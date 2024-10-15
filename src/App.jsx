d050c692da840379b67a07b18ce209fa3097b5a0
David Kwon <david.kwon@clover.com>
2024-10-15 14:08:27 -0600

    Edit home section heading text

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import WizardPortrait from './assets/wizard_portrait.png';
 const App = () => {
   return (
     <main className='h-[85%] w-full flex justify-center items-center font-jetbrains px-24 text-white'>
-      <span className='text-right text-wrap w-[40%]'>
+      <span className='text-right text-pretty w-[40%]'>
         <h2 className='text-3xl mb-4'>Hi, I'm David 👋</h2>
         <h2 className='text-6xl mb-12'>A powerful wizard</h2>
-        <h2 className='text-3xl mb-4'>Wielding the power of Harry Potter himself</h2>
+        <h2 className='text-2xl mb-4'>Wielding the power of Harry Potter and all the legends before him...</h2>
       </span>
       <img src={WizardPortrait} className='rounded-3xl ml-16'/>
     </main>
