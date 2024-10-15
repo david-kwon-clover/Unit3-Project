e5d7dc8fa937019fed2bb0ec2eb28b7681e2d284
David Kwon <david.kwon@clover.com>
2024-10-15 11:17:11 -0600

    Add header tag within main in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 const App = () => {
   return (
-    <main className='h-full w-full'></main>
+    <main className='h-full w-full flex flex-col items-center'>
+      <header className='bg-red-200 h-[15%] w-[90%] mt-2'></header>
+    </main>
   )
 }
 
