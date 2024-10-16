c9afe4e952f5d82b87b6d98922202b24e9e645bd
David Kwon <david.kwon@clover.com>
2024-10-16 10:52:50 -0600

    Add event listener for home image to display a hidden message

--- src/routes/Portfolio.jsx
+++ src/routes/Portfolio.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Portfolio = () => {
   return (
     <section className='h-[85%] w-full flex justify-center items-center'>
-      <p className='text-3xl mb-4 text-white'>Check out more of my work in my <a href='https://dkwon.vercel.app/' className='text-blue-600 dark:text-blue-500 hover:underline'>portfolio</a></p>
+      <p className='text-3xl mb-4 text-white'>Check out more of my work in my <a href='https://dkwon.vercel.app/' className='text-purple-400 hover:underline font-extrabold'>portfolio</a></p>
     </section>
   )
 }
